6e0297c0d6cbef0802b19c8f3ef7b2451d40a59b
imDorian <dorians.developer@gmail.com>
2025-05-17 02:44:04 +0200

    add login page

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -13,32 +13,65 @@ export default function TabsLayout() {
         tabBarShowLabel: true,
         tabBarActiveTintColor: "#fff",
         tabBarInactiveTintColor: "#fff",
-        tabBarIcon: ({ focused, color, size }) => {
-          return (
-            <Ionicons
-              name={focused ? "home" : "home-outline"}
-              size={size}
-              color={color}
-            />
-          );
-        },
         tabBarLabelStyle: {
           fontSize: 12,
         },
       }}
     >
-      <Tabs.Screen name="index" options={{ title: "Inicio" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Inicio",
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "add-circle" : "add-circle-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="income"
-        options={{ title: "Ganancias", headerShown: false }}
+        options={{
+          title: "Ganancias",
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "documents" : "documents-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="chat"
-        options={{ title: "Chat", headerShown: false }}
+        options={{
+          title: "TaxiA",
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "chatbox" : "chatbox-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="settings"
-        options={{ title: "Ajustes", headerShown: false }}
+        options={{
+          title: "Ajustes",
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "settings" : "settings-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
       />
     </Tabs>
   );
--- app/(tabs)/settings.js
+++ app/(tabs)/settings.js
@@ -1,12 +1,21 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import Screen from "../../components/Screen";
+import { useRouter } from "expo-router";
 
 export default function Settings() {
+  const router = useRouter();
+
+  function logout() {
+    router.replace("/login");
+  }
   return (
-    <Screen>
+    <Screen className="">
       <View>
         <Text className="text-white text-3xl font-bold">Setting</Text>
       </View>
+      <Pressable onPress={logout} className="bg-red-400 rounded-2xl p-4">
+        <Text className="text-white text-center">Cerrar Sesion</Text>
+      </Pressable>
     </Screen>
   );
 }
